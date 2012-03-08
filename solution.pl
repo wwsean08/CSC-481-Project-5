e16b58fffecc5382902859877f64129be1599352
Sean Smith <wwsportsguy@gmail.com>
2012-03-08 01:17:56 -0500

    condensed some code, combining the gotoTofu and gotoTarget into a general gotoLocation rule

--- solution.pl
+++ solution.pl
@@ -11,8 +11,10 @@ run:-
 	q_connect(Q),							%connect to the game and spawn a quagent
 	q_cameraon(Q),							%turn on the camera so we look thru the quagents eyes
 	findTofu(Q, X, Y).						%search for the tofu in the world, more information with the rule
-	%gotoTofu(Q, X, Y),						%traverse the map to the tofu location and pick it up, more info with the rule
-	%gotoTarget(Q, targetX, targetY),		%traverse the map to the target location and drop the tofu, more info with the rule
+	%gotoLocation(Q, X, Y),					%traverse the map to the tofu location and pick it up, more info with the rule
+	q_pickup(Q, 'tofu'), 		%pickup the tofu and we're done
+	%gotoLocation(Q, targetX, targetY),		%traverse the map to the target location and drop the tofu, more info with the rule
+	q_drop(Q, 'tofu'),
 	%q_close(Q).							%close the quagent, if we did all this, we are done and worked successfully
 
 %Find tofu uses the radius command to locate the tofu within the room.
@@ -23,12 +25,12 @@ findTofu(Q, X, Y) :-
 	q_events(Q, RadiusEvents),				%grab the event (hopefully)
 	parseRadiusEvent(RadiusEvents, X, Y).	%parse the event
 
-%traverses the world in order to get to the location of the tofu and pick it up.
+%traverses the world in order to get to the location (either the target or the tofu)
 %Step 1: Get our location in the world -partially implemented (maybe?)
 %Step 2: Determine where we have to go along the X and Y axis to get to the tofu
 	%To do this go one axis, turn 90 or -90 and then go the other axis to locate the quagent at the tofu
 %Step 3: Pickup Tofu - Done, that was easy
-gotoTofu(Q, X, Y) :- 
+gotoLocation(Q, X, Y) :- 
 	q_where(Q),					%execute the where event to find out our location
 	q_events(Q,WhereEvent),		%not quite sure if this will work but will try it anyways to get the results of the where
 	parseWhereEvent(WhereEvent),%parses the event returned by q_events
@@ -40,26 +42,6 @@ gotoTofu(Q, X, Y) :-
 	DistanceY is abs(Y - targetY),	%incase we change the target
 	q_turn(Q, Turn),
 	q_walk(Q, DistanceY),
-	q_pickup(Q, 'tofu'). 		%pickup the tofu and we're done
-
-%traverses the world and goes to the target location to drop the tofu
-%Step 1: Get our location in the world -partially implemented (maybe?)	
-%Step 2: Determine where we have to go along the x and y axis to get to the target
-	%To do this go one axis, turn 90 or -90 and then go the other axis to locate the quagent at 0,0
-%Step 3: Drop the tofu - Done, that was easy
-gotoTarget(Q, X, Y) :- 
-	q_where(Q),					%execute the where event to find out our location
-	q_events(Q,WhereEvent),		%not quite sure if this will work but will try it anyways to get the results of the where
-	parseWhereEvent(WhereEvent, X, Y),%parses the event returned by q_events
-	%Determine which way we are looking here that way we know where we need to turn
-	DistanceX is abs(X - targetX),	%incase we change the target
-	q_turn(Q, Turn),
-	q_walk(Q, DistanceX),
-	%Determine which way we are looking here that way we know where we need to turn
-	DistanceY is abs(Y - targetY),	%incase we change the target
-	q_turn(Q, Turn),
-	q_walk(Q, DistanceY),
-	q_drop(Q, 'tofu'). 			%drop the tofu and we are done.
 
 %parse the radius event in order to find the tofu
 parseRadiusEvent([], X, Y) :- nl.
